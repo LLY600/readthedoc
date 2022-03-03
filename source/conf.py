91965a5eb7d4f22dcfcc5ec24c28298e584708e2
杏壳儿 <liuly2@knownsec.com>
2022-03-03 11:01:30 +0800

    add content

--- source/conf.py
+++ source/conf.py
@@ -14,7 +14,9 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 import sphinx_rtd_theme
-
+import recommonmark
+from recommonmark.transform import AutoStructify
+from recommonmark.parser import CommonMarkParser
 
 # -- Project information -----------------------------------------------------
 
@@ -25,14 +27,13 @@ author = 'Liulinyuan'
 # The full version, including alpha/beta/rc tags
 release = '1.0.0'
 
-
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 
-extensions = ['recommonmark','sphinx_markdown_tables'] 
+extensions = ['recommonmark', 'sphinx_markdown_tables']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -49,13 +50,12 @@ language = 'zh_CN'
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
 
-
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #	
-#html_theme = 'alabaster'
+# html_theme = 'alabaster'
 html_theme = 'sphinx_rtd_theme'
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
@@ -63,3 +63,20 @@ html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# At top on conf.py (with other import statements)
+
+
+# At the bottom of conf.py
+source_parsers = {
+    '.md': CommonMarkParser,
+}
+source_suffix = ['.rst', '.md']
+
+
+def setup(app):
+    app.add_config_value('recommonmark_config', {
+        'url_resolver': lambda url: github_doc_root + url,
+        'auto_toc_tree_section': 'Contents',
+    }, True)
+    app.add_transform(AutoStructify)
--- source/index.rst
+++ source/index.rst
@@ -7,10 +7,8 @@ Welcome to Liu圈圈的后宫~ [手动笔芯]
     软件测试体系知识/index
     Python笔记/index
     计算机网络/index
-    Web安全/Nessus
-    Web安全/Awvs
-    Web安全/Nessus/Nessus破解
-    Web安全/Awvs/Awvs破解
+    Web安全/Nessus/index
+    Web安全/Awvs/index
     数据库/index 
     微服务/index
     前端游乐场/index
